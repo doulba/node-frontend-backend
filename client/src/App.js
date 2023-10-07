0f6c587290af3c956de11715e7c5ae03aca19fe5
Abdoul BA <abdoulba@me.com>
2023-10-07 20:53:47 +0200

    show api result

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import Axios from "axios";
 import logo from './logo.svg'
 import './App.css'
 
-function App () {
-  const [msg, setMsg] = useState('')
-  const handleClick = async () => {
-    const data = await window.fetch('/api/users')
-    const json = await data.json()
-    const msg = json.msg
-    setMsg(msg)
-  }
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const { data } = await  await Axios.get('/api/user')
+    const users = await data;
+    setUsers(users);
+    console.log(users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <button onClick={handleClick}>
-          Call api
-        </button>
-        <p>{msg}</p>
-      </header>
+    <div>
+      {users.map((user) => (
+        <p key={user._id}>{user.fullname}</p>
+      ))}
     </div>
-  )
+  );
 }
+
+// function App () {
+//   const [msg, setMsg] = useState('')
+//   const handleClick = async () => {
+//     const data = await window.fetch('/api/user')
+//     const json = await data.json()
+//     const msg = json.msg
+//     setMsg(msg)
+//   }
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <button onClick={handleClick}>
+//           Call api
+//         </button>
+//         <p>{msg}</p>
+//       </header>
+//     </div>
+//   )
+// }
 
 export default App
