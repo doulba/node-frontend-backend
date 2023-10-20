430d20950e7b7fda60d28e7967c8868721db3b5b
Bathie BA <bathieba@gmail.com>
2023-10-20 09:58:30 +0200

    frontend ehancement

--- new_client/src/pages/home.js
+++ new_client/src/pages/home.js
@@ -1,16 +1,30 @@
 
 import '../App.css';
+import { Tilt } from "react-tilt";
 import logo from '../logo.jpeg'
+
 export default function Create() {
 
     return (
-        <div className="main">
-            <h2 className="main-header">Gestion Daara IT</h2>
-            <div className="Tilt-inner login100-pic" data-tilt>
-                <img src={logo}
-                    alt="image" />
+
+        <main>
+            <div className="limiter" >
+                <div className="container-login100" >
+                    <div className="wrap-login100" >
+                        <Tilt className="Tilt"
+                            options={
+                                { max: 100 }
+                            } >
+                            <h2 className="main-header">Gestion Daara IT</h2>
+                            <div className="Tilt-inner login100-pic" data-tilt>
+                                <img src={logo}
+                                    alt="image" />
+                            </div>
+                        </Tilt>
+                    </div>
+                </div>
             </div>
-        </div>
+        </main>
     )
 
 }
